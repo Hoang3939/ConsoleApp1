ee8304d234521f91c2d3d58d6300577a133cba24
Hoang3939 <nguyenhuyhoang0943@gmail.com>
2025-09-20 09:28:36 +0700

    Upload source code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("1 + 1 = " + (1 + 1));
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Xin chào, tôi là {Name}, {Age} tuổi.");
+    }
+}
+
